44f9480cbdf9bb04833aba25829b4d638cd4f62a
malyons <marcus.lyons@klowd.com>
2018-04-09 14:23:10 -0600

    basic tests added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-"use strict";
+// "use strict";
 
 var mailer = require("./mailer.js");
 
--- mailer.js
+++ mailer.js
@@ -1,8 +1,13 @@
-"use strict";
+// 'use strict';
 
 var sendgridMail = require("@sendgrid/mail");
 
 module.exports.run = function(event, context, cb) {
+  // ensure event is set
+  if (!event) {
+    return cb(new Error("EVENT IS NOT DEFINED"));
+  }
+
   // ensure minimum environment variables are set
   if (!process.env.SENDGRID_API_KEY) {
     return cb(new Error("SENDGRID API KEY NOT SET"));
@@ -21,10 +26,18 @@ module.exports.run = function(event, context, cb) {
   const msg = {
     to: process.env.SENDGRID_TO_EMAIL,
     from: process.env.SENDGRID_FROM_EMAIL,
-    subject: context.subject,
-    text: context.text,
-    html: context.message
+    subject: event.info.subject,
+    text: event.info.text,
+    html: event.info.html
   };
 
-  sendgridMail.send(msg);
+  sendgridMail.send(msg, (error, result) => {
+    if (error) {
+      console.log(error.toString());
+      return cb(new Error("MAIL SEND FAILED"));
+    } else {
+      return cb(null, {message: 'success'});
+    }
+  });
+
 };
